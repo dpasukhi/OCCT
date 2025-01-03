d2a7577c91a0d17dcc69d0992d18170dc12de7b5
dpasukhi <dpasukhi@opencascade.com>
2025-01-03 21:48:46 +0000

    Refactor BinMDataStd_GenericExtStringDriver to change SourceType to return a const reference for improved performance

--- src/BinMDataStd/BinMDataStd_GenericExtStringDriver.cxx
+++ src/BinMDataStd/BinMDataStd_GenericExtStringDriver.cxx
@@ -45,9 +45,9 @@ Handle(TDF_Attribute) BinMDataStd_GenericExtStringDriver::NewEmpty() const
 //function : SourceType
 //purpose  : 
 //=======================================================================
-Handle(Standard_Type)& BinMDataStd_GenericExtStringDriver::SourceType() const
+const Handle(Standard_Type)& BinMDataStd_GenericExtStringDriver::SourceType() const
 {
-  return STANDARD_TYPE(TDataStd_GenericExtString);
+  return Standard_Type::Instance<TDataStd_GenericExtString>();
 }
 
 //=======================================================================
