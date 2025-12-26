c86bc30080008ac2f811b4ba38c25142da746ab8
dpasukhi <dpasukhi@opencascade.com>
2025-12-26 10:37:53 +0000

    Modeling - Add license headers to migration scripts for OCCT 8.0.0

--- adm/scripts/migration_800/migrate_handles.py
+++ adm/scripts/migration_800/migrate_handles.py
@@ -3,8 +3,14 @@
 #
 # This file is part of Open CASCADE Technology software library.
 #
-# Specialized script for Handle(Class) -> occ::handle<Class> migration
-# and Handle(Class)::DownCast -> occ::down_cast<Class> migration
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 2.1 as published
+# by the Free Software Foundation, with special exception defined in the file
+# OCCT_LGPL_EXCEPTION.txt. Consult the file LICENSE_LGPL_21.txt included in OCCT
+# distribution for complete text of the license and disclaimer of any warranty.
+#
+# Alternatively, this file may be used under the terms of Open CASCADE
+# commercial license or contractual agreement.
 
 """
 Handle Migration Script for OCCT 8.0.0
--- adm/scripts/migration_800/migrate_standard_types.py
+++ adm/scripts/migration_800/migrate_standard_types.py
@@ -3,7 +3,14 @@
 #
 # This file is part of Open CASCADE Technology software library.
 #
-# Specialized script for Standard_* type migration to native C++ types
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 2.1 as published
+# by the Free Software Foundation, with special exception defined in the file
+# OCCT_LGPL_EXCEPTION.txt. Consult the file LICENSE_LGPL_21.txt included in OCCT
+# distribution for complete text of the license and disclaimer of any warranty.
+#
+# Alternatively, this file may be used under the terms of Open CASCADE
+# commercial license or contractual agreement.
 
 """
 Standard_* Type Migration Script for OCCT 8.0.0
